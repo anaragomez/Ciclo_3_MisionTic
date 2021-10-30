e8c7f0d0bcd517a4cc7661c9747827293071baa0
cristinamejia1402 <cristinamejia1402@gmail.com>
2021-10-30 12:47:46 -0500

    delete files logo192 & logo512; create ProductsStyle

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,20 @@
 import React from 'react';
+
 function Products(){
     return(
-        <div>AQUÍ VA LOS PRODUCTOS</div>
-    )
+            
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            </div>
+            </nav>
+         </div>
+
+ );
 }
 
 export default Products; 
+
+
+
