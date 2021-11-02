b8d7f687f4d8daaf5fea0394b1cc4e1b416b4223
cristinamejia1402 <cristinamejia1402@gmail.com>
2021-11-01 20:51:45 -0500

    enrutamiento de src/components/Signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,27 @@
-import React from 'react';
+import React from "react";
+import Logo from "./components/Logo";
+import navbar from "./components/comunes/navbars";
+
+
+function Signup(){
+    return(
+        <div class="wrapper">
+        <div class="one">One</div>
+        <div class="two">Two</div>
+        </div>
+    );
+}
+
+export default Signup;
+  
+
+
+
+
+
+
+
+
 function Signup(){
     return(
         <div>AQUÍ VA EL REGISTRO</div>
