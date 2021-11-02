289ce436d8a9d138710f75ebc81dc78901b1c306
cristinamejia1402 <cristinamejia1402@gmail.com>
2021-11-01 20:56:16 -0500

    mejoras al slider

---
+++ src/components/comunes/slider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from "./components/Logo";
+
+
+function slider(){
+    return(
+    <nav class="navbar fixed-top navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Fixed top</a>
+    </div>
+    </nav>
+    );
+}
+
+export default slider;
