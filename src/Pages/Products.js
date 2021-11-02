004eb5b7a3e798dbd254bbdee109516d43312d06
cristinamejia1402 <cristinamejia1402@gmail.com>
2021-11-01 19:09:43 -0500

    se modifica subdir Productos/registerProducts por Pages

---
+++ src/Pages/Products.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Products(){
+    return(
+<div class="wrapper">
+  <div class="one">One</div>
+  <div class="two">Two</div>
+  <div class="three">Three</div>
+  <div class="four">Four</div>
+  <div class="five">Five</div>
+  <div class="six">Six</div>
+</div>
+
+        );
+}
+
+export default Products; 
+
+
+
