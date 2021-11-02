be7dada0d5d1c28c314aaa2243ad20e28190a81a
cristinamejia1402 <cristinamejia1402@gmail.com>
2021-11-01 20:57:18 -0500

    enrutamiento a Apps

--- src/App.js
+++ src/App.js
@@ -3,39 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 //*import Signup from "./components/Signup";//
 import Products from "./Pages/Products";
-import Products from "./Pages/maestroProducts";
-
+import maestroProducts from "./Pages/maestroProducts";
 //import Login from "./components/Login";*//
  
 function App() {
   return (
-    <Router>
-      <div className='container'>
-      <h1>Navbar</h1>
-      <hr />
-      <Switch>
-        <Route path="/RegistroProductos">
-          Registro Productos
-        </Route>
-        <Route path="/MaestroProductos">
-          Maestro de Productos
-        </Route>
-        <Route path="/RegistroProductos">
-          Registro Productos
-        </Route>
-        <Route path="/Signup">
-          Registro 
-        </Route>
-        <Route path="/Login">
-          Login
-        </Route>
-        <Route path="/">
-          Inicio
-        </Route>
-      </Switch>
-      </div>
-    </Router>
-  
+    <div>
+      <Products/>
+      <maestroProducts/>
+    </div>
   );
 }
 
