af63a41ab6a883518b9e322f9af1734ac3e7f31b
cristinamejia1402 <cristinamejia1402@gmail.com>
2021-11-02 07:21:42 -0500

    correccion de enrutamiento de src/App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,91 @@
+import React from "react";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-//*import Signup from "./components/Signup";//
-import Products from "./Pages/Products";
-import maestroProducts from "./Pages/maestroProducts";
-//import Login from "./components/Login";*//
- 
-function App() {
+//import Login from "./components/Login";
+//import Signup from "./components/Signup";
+import Products from "../src/Pages/Products";
+import maestroProducts from "../src/Pages/maestroProducts";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+export default function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/maestroProducts">MaestroProducts</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/maestroProducts">
+            <maestroProducts />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
   return (
     <div>
-      <Products/>
-      <maestroProducts/>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>Products</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>MaestroProducts</h2>
+    </div>
+  );
+}
