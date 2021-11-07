eaaf5eace9755db5ff8be4ee2b60af38e298c1a8
cristinamejia1402 <cristinamejia1402@gmail.com>
2021-11-07 15:53:37 -0500

    cambio de nombre de navbar a Nabvar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,44 @@
+import Logo from "./Logo";
+
+
 function Navbar(){
     return(
-        <div class="container-xxl">
-         contenedor1
+<nav className="navbar navbar-light bg-light">
+    <div class="container-fluid">
+    <Logo /> Catimore Coffee  
+         
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">RegistroUsuarios</a>
+        </li>
+     
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Administración Productos
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">RegistroProductos</a></li>
+            <li><a class="dropdown-item" href="#">MaestroProductos</a></li>
+          </ul>
 
-        </div>
-    )
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Administración Ventas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">RegistroVentas</a></li>
+            <li><a class="dropdown-item" href="#">MaestroVentas</a></li>
+          </ul>
+        </li>
+      </ul>
+ 
+    </div>  
+    </div>
+
+</nav>
+)
 }
-
 export default Navbar; 
