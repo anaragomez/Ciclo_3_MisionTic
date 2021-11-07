3d2f44ab8016bc17bbad7266cdbf45c89fb45ce0
cristinamejia1402 <cristinamejia1402@gmail.com>
2021-11-07 06:09:48 -0500

    Navbar avance estructura

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from './Logo';
+
+function Navbar(){
+    return(
+        <div class="container">
+        <div class="row row-cols-3">
+          <div class="col-3"> import './Logo.js'</div>
+          <div class="col-6">Catimore Cooffe</div>
+          <div class="col-3">Column</div>
+          </div>
+       )
+};
+
+export default Navbar;
