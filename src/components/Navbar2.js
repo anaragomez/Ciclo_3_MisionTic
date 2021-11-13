b079ad4f456fba6628d8ebd8e714235997249bb1
cristinamejia1402 <cristinamejia1402@gmail.com>
2021-11-13 18:25:01 -0500

    Modificacion de rama Navbar por navbar2

---
+++ src/components/Navbar2.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar2() {
+  return (
+    <nav class="navbar navbar bg-red">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <Logo alt="" width="30" height="24" />
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar2;
