36c6dd6d07fae5d969e9cb15a4390abebe5f2c3c
cristinamejia1402 <cristinamejia1402@gmail.com>
2021-11-13 19:35:25 -0500

    Navbar2 modificado

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 function Navbar2() {
-  return (
-    <nav class="navbar navbar bg-red">
-      <div class="container">
-        <a class="navbar-brand" href="#">
-          <Logo alt="" width="30" height="24" />
-        </a>
-      </div>
-    </nav>
+    return (
+
+<nav class="navbar navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <Logo/>
+    </a>
+  </div>
+</nav>
   );
 }
 
